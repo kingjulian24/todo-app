3f52677e49f1f67f1992077c53b11a518514d7a1
julian brown <brownjulian2@gmail.com>
2024-08-04 20:52:48 -0400

    Remove complete from view todo info

--- src/components/TodoDetails/TodoCheckBox.js
+++ src/components/TodoDetails/TodoCheckBox.js
@@ -1,14 +1,19 @@
-const TodoCheckBox = ({ todo, handleChange }) => {
+import { STATUS } from "../../hooks/useTodoDetailsState";
+const TodoCheckBox = ({ todo, handleChange, status }) => {
   return (
     <div className="flex items-center">
-      <input
-        type="checkbox"
-        name="completed"
-        checked={todo.completed}
-        onChange={handleChange}
-        className="mr-2"
-      />
-      <label>Completed</label>
+      {status !== STATUS.VIEW && (
+        <>
+          <input
+            type="checkbox"
+            name="completed"
+            checked={todo.completed}
+            onChange={handleChange}
+            className="mr-2"
+          />
+          <label>Completed</label>
+        </>
+      )}
     </div>
   );
 };
