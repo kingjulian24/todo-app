23615cdb3e50e1b75698054b11eb81123edc6d00
julian brown <brownjulian2@gmail.com>
2024-08-05 16:00:14 -0400

    "Align AddTodoButton"

--- src/components/AddTodoButton/index.js
+++ src/components/AddTodoButton/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const AddTodoButton = ({ toggleAdding }) => (
   <button
     onClick={toggleAdding}
-    className="bg-green-500 rounded-full fixed bottom-5 left-1/2 lg:left-5/6 -translate-x-5/6 z-10 shadow-xl"
+    className="bg-green-500 rounded-full fixed bottom-5 left-1/2  -translate-x-1/2  lg:translate-x-1/2 z-10 shadow-xl"
   >
     <svg
       className="w-16 h-16 text-white"
