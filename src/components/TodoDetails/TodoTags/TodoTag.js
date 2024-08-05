eb6d51ecdb3053a34ca5b569642c084e439703dd
julian brown <brownjulian2@gmail.com>
2024-08-04 20:55:20 -0400

    "Updated TodoTag component to add uuid key, IoMdPricetag icon, and conditional rendering of remove button based on status"

--- src/components/TodoDetails/TodoTags/TodoTag.js
+++ src/components/TodoDetails/TodoTags/TodoTag.js
@@ -1,16 +1,25 @@
 import { FaTrash } from "react-icons/fa";
+import { STATUS } from "../../../hooks/useTodoDetailsState";
+import { v4 as uuidv4 } from "uuid";
+import { IoMdPricetag } from "react-icons/io";
 
-const TodoTag = ({ tag, handleTagRemove }) => {
+const TodoTag = ({ tag, handleTagRemove, status }) => {
   return (
-    <span key={tag} className="bg-gray-200 px-2 py-1 rounded flex items-center">
+    <span
+      key={uuidv4()}
+      className="bg-gray-200 px-2 py-1 rounded flex items-center"
+    >
+      <IoMdPricetag className="mr-1 text-orange-400 text-lg" />
       {tag}
-      <button
-        type="button"
-        onClick={() => handleTagRemove(tag)}
-        className="ml-2 text-red-500"
-      >
-        <FaTrash size="12" />
-      </button>
+      {status !== STATUS.VIEW && (
+        <button
+          type="button"
+          onClick={() => handleTagRemove(tag)}
+          className="ml-2 text-red-500"
+        >
+          <FaTrash size="12" />
+        </button>
+      )}
     </span>
   );
 };
